6de962718253adf042076ef0106e3103942df476
Umdyne <103336752+Umdyne@users.noreply.github.com>
2023-11-02 11:30:14 -0300

    Troca-de-API

--- pages/index.vue
+++ pages/index.vue
@@ -8,11 +8,11 @@
       <v-main>
         <v-container class="bg-grey-darken-1">
           <ul>
-            <template v-for="dado in dados.articles" >
+            <template v-for="dado in dados.items" >
               <v-card class="bg-grey-darken-3 ma-5">
-                <v-card-title>{{ dado.title }}</v-card-title>
-                <v-card-subtitle>{{ dado.description }}</v-card-subtitle>
-                <v-card-text>{{ dado.content }}</v-card-text>
+                <v-card-title>{{ dado.titulo }}</v-card-title>
+                <v-card-subtitle>{{ dado.data_pulblicacao }}</v-card-subtitle>
+                <v-card-text>{{ dado.introducao }}</v-card-text>
                 <v-card-actions class="border border-white">
                   <v-btn :href="dado.url" target="_blank">
                     Ir para a notícia
@@ -29,15 +29,15 @@
   <script setup>
 
   
-  const dados = ref({ articles: [] });
+  const dados = ref({ items: [] });
   
   async function recebe() {  
-    const response = await fetch(`https://newsapi.org/v2/everything?domains=wsj.com&apiKey=b991ab4e7d1c4a1a87468a17fcc49f82`);
+    const response = await fetch(`http://servicodados.ibge.gov.br/api/v3/noticias/`);
     const valores = await response.json();
     dados.value = valores;
   }
   onMounted(async () => {
     await recebe();
-    console.log(dados.articles);
+    console.log(dados.items);
   });
   </script>
